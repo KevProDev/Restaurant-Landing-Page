14dfb2f273f055760ed626ae3734c8fb93ab5160
developerXdecoded <34496647+developerXdecoded@users.noreply.github.com>
2019-04-13 13:45:55 -0500

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
-import '../../node_modules/jquery/dist/jquery';
+import Jquery from '../../node_modules/jquery/dist/jquery';
 console.log('I AM WORKING');
+
+const $ = Jquery;
+
+$('.menu').on('click', function() {
+	$(this).toggleClass('active');
+	$('.overlay').toggleClass('menu-open');
+});
